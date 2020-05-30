232bda2d22d8457e25288fb4ef6f3a17c01c22a6
Marco Ravelo <mravelo5874@gmail.com>
2020-05-30 12:59:37 -0500

    Added simple main menu

---
+++ UnityProject/NotTowerFall/Assets/Scripts/SceneManagers/MainMenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnlineMultButton()
+    {
+        Debug.Log("OnlineMultButton: Pressed");
+    }
+
+    public void LocalMultButton()
+    {
+        Debug.Log("LocalMultButton: Pressed");
+        SceneManager.LoadScene("DefaultScene");
+    }
+
+    public void SettingsButton()
+    {
+        Debug.Log("SettingsButton: Pressed");
+    }
+
+    public void ExitButton()
+    {
+        Debug.Log("ExitButton: Pressed");
+        Application.Quit();
+    }
+}
